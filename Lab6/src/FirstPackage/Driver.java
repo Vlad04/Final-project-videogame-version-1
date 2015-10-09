9f0cabfa66550525ab78890b75e9a6fad0cfbf00
Vlad04 <Vlad04@users.noreply.github.com>
2015-10-09 15:23:07 +0200

    Update Driver.java

--- Lab6/src/FirstPackage/Driver.java
+++ Lab6/src/FirstPackage/Driver.java
@@ -1,59 +1,129 @@
 package FirstPackage;
 
-public class Square implements Comparable<Square> {
-	protected int Side;									//Side variable of int type, represents the sides of the Square
-	
-	
-	public Square(int side){							//Constructor of Square class that get a side as a parameter
-		this.Side = side;								//initialize the value of the Side
-	}
-	
-	
-	public int perimeter(){								//perimeter method of int type
-		return Side*4;									//returns the perimeter of the square
-	}
-	
-	
-	public int area(){									//area method with empty parameters
-		return Side*Side;								//returns the area of the square
-	}
-	
-	
-	public String toString(){								//A toString method that returns the square
-		return "Square with size " + this.Side + " cm";		//returns the square with an specific size
-	}
-	
-	
-	public boolean equals(Object object){					//Boolean method that check if the Object is equals
-															//and get as parameter an object
+
+import java.lang.Character.UnicodeBlock;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+
+public class Driver {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to my unboundedlist proyect. \n");
+		System.out.println("You have the options to manipulate 2 diferent lists. \n");
+		System.out.println("The first one is a list if cards and the second a list of squares. \n");
+		System.out.println("The first one is a list if cards and the second a list of squares. \n");
+		System.out.println("You have the options to add, remove and count the number of items on every list. \n");
 		
-		if(object!=null){									//If Object is different from null	
-			if(object instanceof Square){					//and then, if object is of type Square (for this we use instanceof
-															//to check that, and that is new for me, so I learn something new as
-															//I said.	
-				
-				Square square=(Square)object;					//Then we create an object of Square type.
-				if(square.Side==this.Side){						//So, if the side of the square is equal to actual side.	
-							
-					return true;								//returns true
+		UnsortedList<Square> UnsortedCardList = new UnsortedList<Square>();
+		UnsortedList<Square> UnsortedSquareList = new UnsortedList<Square>();
+		Scanner sc = new Scanner(System.in);
+		boolean open = true;
+		
+		do {
+			try {
+				System.out.println("Please select an option. \n");
+				System.out.printf("%-10s %10s %n", "[1]", "Add a random value to the a list of cards");
+				System.out.printf("%-10s %10s %n", "[2]", "Add a value the a list of Squares");
+				System.out.printf("%-10s %10s %n", "[3]", "remove a value the list of cards");
+				System.out.printf("%-10s %10s %n", "[4]", "remove a value the list of Squares");
+				System.out.printf("%-10s %10s %n", "[5]", "Show the number of elements of the list of cards");
+				System.out.printf("%-10s %10s %n", "[6]", "Show the number of elements of the list of squares\n");
+				System.out.printf("%-10s %10s %n", "[9]", "Closes the program");
+
+				int options = sc.nextInt();
+
+				switch (options) {
+				case 1:
+					Card commonCard = new Card();
+					UnsortedCardList.add(commonCard);
+					System.out.println("Random card added to the list.");
+					System.out.println("**" + commonCard + "**");
+					break;
+				case 2:
+					Square commonSquare = null;
+					int sideLength = 0;
+					do {
+						try {
+							System.out.println("Input the length for the side of the square");
+							sideLength = sc.nextInt();
+							commonSquare = new Square(sideLength);
+						} catch (InputMismatchException e) {
+							System.err.println("Please enter a number");
+						}
+						sc.nextLine();
+					} while (sideLength <= 0);
+					UnsortedSquareList.add(commonSquare);
+					System.out.println("Square added to the list.");
+					System.out.println("**" + commonSquare + "**");
+					break;
+				case 3:
+					int cardRank = 0, cardSuit = 0;
+					do {
+						try {
+							System.out.println("Input the rank of the card.\n");
+							cardRank = sc.nextInt();
+
+						} catch (InputMismatchException e) {
+							System.err.println("The card rank is invalid.");
+						}
+						sc.nextLine();
+					} while (cardRank<=0 || cardRank>13);
+					do {
+						try {
+							System.out.println("Input the Suit of the card.\n[1]clubs\n[2]diamonts\n[3]hearths\n[4]spades");
+							cardSuit = sc.nextInt();
+						} catch (InputMismatchException e) {
+							System.err.println("The card suit is invalid.");
+						}
+						sc.nextLine();
+					} while (cardSuit <=0 || cardSuit>4);
+					if (UnsortedCardList.remove(new Card(cardRank,cardSuit))) {
+						System.out.println("Card \"" + new Card(cardRank,cardSuit) + "\" REMOVED");
+					} else {
+						System.out.println("Card \"" + new Card(cardRank,cardSuit) + "\" NOT FOUND");
+
+					}
+					break;
+					
+				case 4:
+					sideLength = 0;
+					do {
+						try {
+							System.out.println("Enter the size of the square you want to remove from the list.");
+							sideLength = sc.nextInt();
+
+						} catch (InputMismatchException e) {
+							System.err.println("Please enter a number");
+						}
+						sc.nextLine();
+					} while (sideLength <= 0);
+					if (UnsortedSquareList.remove(new Square(sideLength))) {
+						System.out.println("object \"Square\" REMOVED");
+					}
+					else {
+						System.out.println("Object \"Square\" NOT FOUND");
+					}
+					break;
+				case 5:
+					System.out.println(UnsortedCardList);
+					System.out.println("The number of elements in this list is " + UnsortedCardList.size()+"\n");
+					break;
+				case 6:
+					System.out.println(UnsortedSquareList);
+					System.out.println("The number of elements in this list is " + UnsortedSquareList.size()+"\n");
+					break;
+				case 9:
+					System.out.println("See you later :)");
+					open = false;
+					break;
 				}
-			}
-		}
-		return false;										//if not, returns false
+			} catch (InputMismatchException e) {
+				System.err.println("Inconrrect input\n");
+			} 
+			sc.nextLine();
+		} while (open);
+		sc.close();
 	}
-	
-	
-	public int compareTo(Square square) {					//compareTo method, that is focused in compare the square object.	
-		if (this.Side < square.Side) {						//If this actual side is less than the side of the square.	
-			return -1;										//returns a -1
-		} else if(this.Side == square.Side) {				//Else, if the actual side is equal to the side of the square
-			return 0;										//Then returns a 0
-		}
-		else{												//Else
-			return 1;										//returns 1
-		}
-	}
+
 }
-																
-							
-														
