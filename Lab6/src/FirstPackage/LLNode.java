647878b7c4ec692bf00e17c33a03f1b30f23e162
Vlad04 <Vlad04@users.noreply.github.com>
2015-10-09 14:45:08 +0200

    Update LLNode.java

--- Lab6/src/FirstPackage/LLNode.java
+++ Lab6/src/FirstPackage/LLNode.java
@@ -1,35 +1,50 @@
 package FirstPackage;
 
-
+/*
+Made with the help of the book Object-Oriented Data Structures using Java (3ed edition)
+*/
 public class LLNode<T>{
-	
-	private LLNode<T> link;			
-	private T info;					//Variable named "info" of type T.
-  
-	public LLNode(T info){			//Constructor where we initialize the information and the link.
-    
+
+	private LLNode link;
+	private T info;
+
+	//Pre: None
+	//Post: None
+	public LLNode(){
+		this.info = null;
+		link=null;
+	}
+
+	//None
+	//this.info equals to info
+	public LLNode(T info){
 		this.info = info;
 		link = null;
-	
 	}
- 
-	public void setInfo(T info){		//Set the information of this node.
-		
+	//Pre: None
+	//Post: Set infor of this LLNode
+	public void setInfo(T info){
+	// Sets info of this LLNode.
 		this.info = info;
 	}
 
-	public T getInfo(){				//Get the information of this node.
-		
+	//Pre: None
+	//Post: Return the info of the link
+	public T getInfo(){
+	// Returns info of this LLONode.
 		return info;
 	}
- 
-	public void setLink(LLNode<T> link){	//Set the link of this node.
-  
+	//Pre: None
+	//Post: Sets link of this LLNode.
+	public void setLink(LLNode<T> link){
+	// Sets link of this LLNode.
 		this.link = link;
 	}
 
-	public LLNode<T> getLink(){		//Return the link of this node.
-		
+	//Pre: None
+	//Post: Returns link of this LLNode.
+	public LLNode<T> getLink(){
+	// Returns link of this LLNode.
 		return link;
 	}
 }
